2e1734689c3e8e615530e7adf7a86a4664ecbd57
Tndevfactory <tndevfactory@gmail.com>
2021-12-31 23:08:21 +0100

    md

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-A simple command-line application.
+A medical application for diabete detection
--- bin/test.dart
+++ bin/test.dart
@@ -1,5 +1,5 @@
 void main(List<String> arguments) {
-  print('Hello world!');
+  print('Hello world!!');
   List ch=[1,2,3];
   print(ch);
 }
